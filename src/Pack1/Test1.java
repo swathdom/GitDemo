a1f4bb57f340ad96208e04f6a4a8aab722955e9b
swathdom <swathdom@192.168.1.28>
2022-12-15 23:48:13 -0800

    First Commit

---
+++ src/Pack1/Test1.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		 String a = "xyz";
+		 
+		 if (a == "xyz") {
+			 System.out.println("Valid");
+		 }else {
+			 System.out.println("Invalid");
+		 }
+
+	}
+
+}
